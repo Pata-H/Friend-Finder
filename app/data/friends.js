243c9c42c9d287cd2b22fd5f86bc57d64fb6e517
Pata-H <Pata.Hikari@gmail.com>
2018-06-23 13:59:10 -0700

    the best i can do

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var friendsArr = [
+	{
+		name: "Mark Saners",
+		photo: "https://i.imgur.com/ytSVsZJ.png?size=200",
+		scores: ['5','5','5','5','5','5','5','5','5','5']
+	}, 
+	{
+		name: "Sarah Mailer",
+		photo: "https://i.imgur.com/undefined.png?size=200",
+		scores: ['3','3','3','3','3','3','3','3','3','3']
+	}, 
+	{
+		name: "Jason Jackson",
+		photo: "https://i.imgur.com/VtauW80.jpg?size=200",
+		scores: ['1','1','1','1','1','1','1','1','1','1']
+	}
+]
+
+module.exports = friendsArr;
