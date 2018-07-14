ace69320d6fdcddd7172918db40e890d01eeffb6
Pata-H <Pata.Hikari@gmail.com>
2018-07-14 00:26:09 -0700

    Let's try now

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,9 +4,13 @@ var path = require('path');
 module.exports = function(app) {
 	app.get("/survey", function(req,res) {
 		res.sendFile(path.join(__dirname, "../public/survey.html"));
-	})
+	});
 
-	app.use(function (req, res){
-		res.sendFile(path.join(__dirname, "../public/home.html"));
-	})
-}
+	app.use("/index", function (req, res){
+		res.sendFile(path.join(__dirname, "../public/index.html"));
+	});
+
+	app.get("*", function(req, res) {
+		res.sendFile(path.join(__dirname, "../public/index.html"));
+	  });
+};
